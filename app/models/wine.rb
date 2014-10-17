d931b0a79cfc5026a14411705bc60e2dd260e827
Sean_Cheetham <skc010@bucknell.edu>
2014-10-17 00:19:28 -0400

    rails hw complete

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -9,6 +9,11 @@ class Wine < ActiveRecord::Base
 	has_many :log_entries, dependent: :destroy
 
 	def average_rating
-		log_entries.average(:rating)
+	
+		if log_entries.loaded?
+    		log_entries.map(&:rating).compact.average
+  		else
+    		log_entries.average(:rating)
+  		end
 	end
 end
